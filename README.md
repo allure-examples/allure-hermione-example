8bb9c38904ed06d082b86da433d25efe0563cdca
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-01-30 20:12:42 +0300

    updated tests, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# allure-hermione-example
+# Integration with Hermione Framework
+
 example of the integration with Hermione framework
+
+## Execution
+
+Install the dependencies
+
+```shell
+npm i
+```
+
+Run the example's tests
+
+```shell
+npm t
+```
+
+## Allure Framework repository for Hermione integration
+
+Please visit [the package page](https://github.com/allure-framework/allure-js/blob/master/packages/allure-hermione/README.md)
--- tests/desktop/startpageFailed.mjs
+++ tests/desktop/startpageFailed.mjs
@@ -10,6 +10,10 @@ it("doesn't show any result item", async ({ browser, currentTest }) => {
 	const items = await browser.$$(".w-gl__result")
     const screenshot = await browser.takeScreenshot()
 
+	await browser.feature(currentTest.id(),"Integration with Hermione test framework")
+	await browser.story(currentTest.id(),"Failing tests")
+	await browser.label(currentTest.id(),"service", "Useless failing assert")
+
     await browser.attach(currentTest.id(), screenshot, "image/png")
 
 	expect(items.length).eq(0)
